2016b3af16bcb17ae06cdcd957ec1605dfa82189
Isaac Vasquez <ivasquezr@unicartagena.edu.co>
2026-09-04 00:46:11 -0500

    agrego indices a la matriz

--- src/main/java/SistemaCine.java
+++ src/main/java/SistemaCine.java
@@ -1,36 +1,52 @@
+
 public class SistemaCine {
 
     public static void main(String[] args) {
 
-        System.out.println("=== SISTEMA DE ASIGANCIION DE ASIENTOS ===");
+        System.out.println("=== SISTEMA DE ASIGNACIÓN DE ASIENTOS ===");
 
         String[][] sala = new String[5][8];
 
+        // Llenar la sala con D = Disponible
         for (int fila = 0; fila < sala.length; fila++) {
 
             for (int columna = 0; columna < sala[fila].length; columna++) {
 
                 sala[fila][columna] = "D";
 
-
-
             }
 
         }
 
+
+        System.out.print("       ");
+
+        for (int columna = 0; columna < sala[0].length; columna++) {
+
+            System.out.print((columna + 1) + "  ");
+
+        }
+
+        System.out.println();
+
+
         for (int fila = 0; fila < sala.length; fila++) {
+
+            System.out.print("Fila " + (fila + 1) + " ");
 
             for (int columna = 0; columna < sala[fila].length; columna++) {
 
-                System.out.print(sala[fila][columna] + " ");
+                System.out.print(sala[fila][columna] + "  ");
 
             }
 
             System.out.println();
+
         }
 
     }
 
 }
+
 
 
